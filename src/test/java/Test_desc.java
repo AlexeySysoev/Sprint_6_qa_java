5fabe8a7e99e78d4befb4772acae721b927e83ca
Alexey Sysoev <wowregfatgod@yandex.ru>
2022-11-18 14:49:48 +0300

    description test cover

---
+++ src/test/java/Test_desc.java
@@ -0,0 +1,24 @@
+public class Test_desc {
+    //class Cat cover
+    //getSound() - return "Мяу"
+    //getFood() -  return predator.eatMeat()
+
+    //class Feline cover
+    // eatMeat - return getFood("Хищник")
+    //getFamily() - return "Кошачьи";
+    //getKittens() - return getKittens(1);
+    //getKittens(int kittensCount) - return kittensCount;
+
+    //class Lion cover
+    // Lion(String sex) throws Exception
+        //"Самец" - hasMane = true;
+        //"Самка" - hasMane = false;
+        //throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+
+    //class Animal cover
+    //getFood(String animalKind) throws Exception
+        //"Травоядное" - return List.of("Трава", "Различные растения");
+        //"Хищник" - return List.of("Животные", "Птицы", "Рыба");
+        // throw new Exception("Неизвестный вид животного, используйте значение Травоядное или Хищник");
+    //String getFamily() - return "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+}
